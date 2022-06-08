0211b1fa20ae8f13381787baf355a17a8d9d4afb
Fausto Lopez <lopezfausto123@yahoo.com>
2022-06-08 10:46:22 -0700

    todo's

---
+++ server/models/MenuItem.js
@@ -0,0 +1,11 @@
+/*
+    TODO: Define mongoose (mongodb) schema for a menu item
+
+    // name
+    // price
+    // city <-- I would put this in the User schema instead of here if you want to query users/companies by city
+    // reference to User
+*/
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
